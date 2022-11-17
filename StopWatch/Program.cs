8889317e1642d53f7997236da6ea50b0da76ed0a
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-17 19:47:40 +0530

    Stop Watch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hi");
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed :{0}", stopwatch.Elapsed);
+        }
+    }
+}
